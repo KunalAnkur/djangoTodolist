f72954a4e284430a73ce2203cffdb36ba6195bf1
Ankur Kunal <mrankur@gmail.com>
2020-03-15 04:26:47 +0530

    implemented create and delete operation

--- backend/src/articles/api/urls.py
+++ backend/src/articles/api/urls.py
@@ -1,9 +1,27 @@
-from django.urls import path
+from articles.api.views import ArticleViewSet
+from rest_framework.routers import DefaultRouter
 
-from .views import ArticleListView, ArticleDetailView
+router = DefaultRouter()
+router.register(r'', ArticleViewSet)
+urlpatterns = router.urls
 
 
-urlpatterns = [
-    path('', ArticleListView.as_view()),
-    path('<pk>', ArticleDetailView.as_view())
-]
+
+
+
+# from django.urls import path
+
+# from .views import (
+#     ArticleListView, 
+#     ArticleDetailView,
+#     ArticleCreateView,
+#     ArticleDeleteView
+# )
+
+
+# urlpatterns = [
+#     path('', ArticleListView.as_view()),
+#     path('create/', ArticleCreateView.as_view()),
+#     path('<pk>', ArticleDetailView.as_view()),
+#     path('<pk>/delete/', ArticleDeleteView.as_view())
+# ]
